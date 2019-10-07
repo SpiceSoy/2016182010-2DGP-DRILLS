d8d4240edc0741cd1e1b88ad7df6bded796f4180
SpiceSoy_laptop <aksghkgh12@gmail.com>
2019-10-07 18:53:56 +0900

    플레이어 이동 업데이트 함수 선언

--- Drill-05/code.py
+++ Drill-05/code.py
@@ -20,7 +20,8 @@ player_source_y = player_pos_y
 
 player_is_running = False
 player_is_view_left = False
-
+player_running_i = 0
+player_running_i_max = 100
 
 def input_handling():
     global running
@@ -30,6 +31,8 @@ def input_handling():
     global player_pos_y
     global player_dest_x
     global player_dest_y
+    global player_is_running
+    global player_running_i
 
     events = pico2d.get_events()
     for event in events:
@@ -38,10 +41,31 @@ def input_handling():
             player_source_x = player_pos_y
             player_dest_x = event.x
             player_dest_y = event.y
+            player_is_running = 0
             player_is_running = True
         elif event.type == pico2d.SDL_KEYDOWN and event.key == pico2d.SDLK_ESCAPE:
             running = False
+
     pass
+
+def update_move():
+    global player_source_x
+    global player_source_y
+    global player_pos_x
+    global player_pos_y
+    global player_dest_x
+    global player_dest_y
+    global player_is_running
+    global player_running_i
+    global player_running_i_max
+    if player_is_running:
+        t = player_running_i / player_running_i_max
+        player_pos_x = (1-t) * player_source_x + t * player_dest_x
+        player_pos_y = (1-t) * player_source_y + t * player_dest_y
+        player_running_i = player_running_i + 1
+        if player_running_i > player_running_i_max:
+            player_running_i  = 0
+            player_is_running = False
 
 while running:
     back_img.draw(width/2, heigth/2)
