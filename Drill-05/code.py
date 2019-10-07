a68fe73f34fa79fa1164b1bff7983b1bfa88bcc6
SpiceSoy_laptop <aksghkgh12@gmail.com>
2019-10-07 18:48:14 +0900

    마우스 인풋 핸들 선언 추가

--- Drill-05/code.py
+++ Drill-05/code.py
@@ -9,14 +9,40 @@ running = True
 
 back_img = pico2d.load_image("KPU_GROUND.png")
 
-# def input_handling():
-#     events = pico2d.get_events()
-#     for event in events:
-#         if event.type == pico2d.SDL_MOUSEBUTTONDOWN and event.button == pico2d.SDL_BUTTON_LEFT:
+player_pos_x = width/2
+player_pos_y = heigth/2
 
+player_dest_x = player_pos_x
+player_dest_y = player_pos_y
+
+player_source_x = player_pos_x
+player_source_y = player_pos_y
+
+player_is_running = False
+player_is_view_left = False
+
+
+def input_handling():
+    global player_source_x
+    global player_source_y
+    global player_pos_x
+    global player_pos_y
+    global player_dest_x
+    global player_dest_y
+
+    events = pico2d.get_events()
+    for event in events:
+        if event.type == pico2d.SDL_MOUSEBUTTONDOWN and event.button == pico2d.SDL_BUTTON_LEFT:
+            player_source_x = player_pos_y
+            player_source_x = player_pos_y
+            player_dest_x = event.x
+            player_dest_y = event.y
+            player_is_running = True
+    pass
 
 while running:
     back_img.draw(width/2, heigth/2)
     pico2d.update_canvas()
+    input_handling()
 
 pico2d.close_canvas()
