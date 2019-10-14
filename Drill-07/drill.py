2434620871dde4dc19ede57c076a5978e1013843
SpiceSoy_laptop <aksghkgh12@gmail.com>
2019-10-14 18:37:21 +0900

    Drill 07 Init

---
+++ Drill-07/drill.py
@@ -0,0 +1,59 @@
+from pico2d import *
+import random
+
+
+class Grass:
+    def __init__(self):
+        self.image = pico2d.load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = pico2d.load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+# initialization code
+pico2d.open_canvas()
+grass = Grass()
+team = [Boy() for n in range(10)]
+running = True
+# game main loop code
+while running:
+    handle_events()
+    for boy in team:
+        boy.update()
+
+    clear_canvas()
+    grass.draw()
+
+    for boy in team:
+        boy.draw()
+
+    update_canvas()
+    delay(0.05)
+
+# finalization code
+
+close_canvas()
